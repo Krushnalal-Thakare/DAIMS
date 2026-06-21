78ce74c7566732f4ebb05f336578a378a285a5af
Krushnalal thakare <krushnalalthkare@gmail.com>
2026-06-21 19:41:38 +0530

    First Version

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div>
+      <h1>
+        Dead Animal & Injured Animal <br /> <br /> Management System
+      </h1>
+
+      <div>
+        <h1>Report Animal</h1>
+
+        <input placeholder="Name" />
+        <br /><br />
+
+        <input placeholder="Mobile Number" />
+        <br /><br />
+
+        <input placeholder="Animal Name" />
+        <br /><br />
+
+        <textarea placeholder="Description"></textarea>
+        <br /><br />
+        
+        <input type="file" />
+
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
