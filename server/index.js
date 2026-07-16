6efe6bbd18c1892c84ee3a7510553d2390986f7a
Krushnalal thakare <krushnalalthkare@gmail.com>
2026-07-16 11:01:40 +0530

    Use Gmail SMTP 587

--- server/index.js
+++ server/index.js
@@ -29,13 +29,12 @@ mongoose
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // 587 साठी false
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 transporter.verify(function (error, success) {
